d38cbc4ad5d71a25ece6092f388163bc3e0a0a67
Sazails <armanis999@gmail.com>
2020-07-22 13:31:38 +0100

    Automated project update

--- Spring_Boot_CRUD/src/main/java/org/sazails/dao/UserRepository.java
+++ Spring_Boot_CRUD/src/main/java/org/sazails/dao/UserRepository.java
@@ -2,6 +2,8 @@ package org.sazails.dao;
 
 import org.sazails.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 // JpaRepository gives all the CRUD methods
+//@RepositoryRestResource(path = "members") this replaces the default "/users" to "/members"
 public interface UserRepository extends JpaRepository<User, Integer> { }
