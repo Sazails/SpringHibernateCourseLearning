84d1370dbc3853bacf4879bc9def5097be2eeead
Sazails <armanis999@gmail.com>
2020-07-22 13:00:41 +0100

    Using Spring Data JPA only

--- Spring_Boot_CRUD/src/main/java/org/sazails/service/UserServiceImpl.java
+++ Spring_Boot_CRUD/src/main/java/org/sazails/service/UserServiceImpl.java
@@ -12,23 +12,12 @@ import java.util.Optional;
 @Service
 public class UserServiceImpl implements UserService {
 
-//    private UserDAO userDAO;
     private UserRepository userRepository;
 
     @Autowired
     public UserServiceImpl(UserRepository userRepository){
         this.userRepository = userRepository;
     }
-
-    /*@Autowired
-    public UserServiceImpl(@Qualifier("userDAOJpaImpl")UserDAO userDAO){
-        this.userDAO = userDAO;
-    }*/
-
-    /*@Autowired
-    public UserServiceImpl(UserDAO userDAO){
-        this.userDAO = userDAO;
-    }*/
 
     @Override
     @Transactional
@@ -37,7 +26,6 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-//    @Transactional
     public User find(int id) {
         Optional<User> user = userRepository.findById(id);
 
@@ -52,38 +40,12 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-//    @Transactional
     public void save(User user) {
         userRepository.save(user);
     }
 
     @Override
-//    @Transactional
     public void delete(int id) {
         userRepository.deleteById(id);
     }
-
-    /*@Override
-    @Transactional
-    public List<User> findAll() {
-        return userDAO.findAll();
-    }
-
-    @Override
-    @Transactional
-    public User find(int id) {
-        return userDAO.find(id);
-    }
-
-    @Override
-    @Transactional
-    public void save(User user) {
-        userDAO.save(user);
-    }
-
-    @Override
-    @Transactional
-    public void delete(int id) {
-        userDAO.delete(id);
-    }*/
 }
